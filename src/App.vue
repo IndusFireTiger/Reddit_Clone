a340b245e5f28099e9ec2ca6deabb2d1da266a03
indusfiretiger <indusfiretiger@gmail.com>
2018-04-23 23:13:45 +0530

    Primitive slide for comment component

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="poster">
     <homeMenu></homeMenu>
     <navbar></navbar>
+    <span style="font-size:30px;cursor:pointer" v-on:click="openNav()">&#9776; open</span>
     <!-- <p>subred {{$route.params.subred_id}}</p>
     <p>user {{$route.params.user_id}}</p>
     <p>cat {{$route.params.cat_id}}</p> -->
@@ -10,7 +11,9 @@
       <router-view></router-view>
       <side id=side-div></side>
     </div>
-    
+    <div id="mySidenav" class="sidenav">
+      <a href="javascript:void(0)" class="closebtn" v-on:click="closeNav()">&times;</a>
+    </div>
   </div>
 </template>
 
@@ -28,6 +31,12 @@ export default {
     goto() {
       console.log('goto')
       this.$router.push('/r/politics')
+    },
+    openNav() {
+        document.getElementById("mySidenav").style.width = "73%";
+    },
+    closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
     }
   },
   watch: {
@@ -39,6 +48,7 @@ export default {
     console.log('beforeRouteUpdate-------------------', from.path, to.path)
   }
 }
+
 </script>
 
 <style>
@@ -56,5 +66,45 @@ body{
   display: grid;
   grid-template: 'post-div side-div';
   padding:5px;
+}
+
+.sidenav {
+    height: 100%;
+    width: 0;
+    position: fixed;
+    z-index: 1;
+    top: 0;
+    left: 0;
+    /* background-color: white; */
+    background: linear-gradient(to bottom right, white,rgb(93, 250, 250));
+    overflow-x: hidden;
+    transition: 0.8s;
+    padding-top: 60px;
+}
+
+.sidenav a {
+    padding: 8px 8px 8px 32px;
+    text-decoration: none;
+    font-size: 25px;
+    color: #818181;
+    display: block;
+    transition: 0.5s;
+}
+
+.sidenav a:hover {
+    color: #f1f1f1;
+}
+
+.sidenav .closebtn {
+    position: absolute;
+    top: 0;
+    right: 25px;
+    font-size: 36px;
+    margin-left: 50px;
+}
+
+@media screen and (max-height: 450px) {
+  .sidenav {padding-top: 15px;}
+  .sidenav a {font-size: 18px;}
 }
 </style>
