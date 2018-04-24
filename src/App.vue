367892afdefe3f436cd8f96559e5d146d0469fbb
indusfiretiger <indusfiretiger@gmail.com>
2018-04-24 08:54:53 +0530

    moved test code for comment into relevant file

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,16 @@
   <div id="poster">
     <homeMenu></homeMenu>
     <navbar></navbar>
-    <span style="font-size:30px;cursor:pointer" v-on:click="openNav()">&#9776; open</span>
     <!-- <p>subred {{$route.params.subred_id}}</p>
     <p>user {{$route.params.user_id}}</p>
     <p>cat {{$route.params.cat_id}}</p> -->
+    <comment></comment>
     <div class='container'>
       <post id=post-div></post>
       <router-view></router-view>
       <side id=side-div></side>
     </div>
-    <div id="mySidenav" class="sidenav">
-      <a href="javascript:void(0)" class="closebtn" v-on:click="closeNav()">&times;</a>
-    </div>
+    
   </div>
 </template>
 
@@ -23,21 +21,15 @@ import post from "./components/post";
 import navbar from "./components/navbar";
 import homeMenu from "./components/home";
 import side from "./components/side";
-
+import comment from "./components/comment"
 export default {
   name: "poster",
-  components: {homeMenu, navbar, post, side},
+  components: {homeMenu, navbar, post, side, comment},
   methods: {
     goto() {
       console.log('goto')
       this.$router.push('/r/politics')
-    },
-    openNav() {
-        document.getElementById("mySidenav").style.width = "73%";
-    },
-    closeNav() {
-        document.getElementById("mySidenav").style.width = "0";
-    }
+    }    
   },
   watch: {
     '$route': function(from, to) {
@@ -68,43 +60,4 @@ body{
   padding:5px;
 }
 
-.sidenav {
-    height: 100%;
-    width: 0;
-    position: fixed;
-    z-index: 1;
-    top: 0;
-    left: 0;
-    /* background-color: white; */
-    background: linear-gradient(to bottom right, white,rgb(93, 250, 250));
-    overflow-x: hidden;
-    transition: 0.8s;
-    padding-top: 60px;
-}
-
-.sidenav a {
-    padding: 8px 8px 8px 32px;
-    text-decoration: none;
-    font-size: 25px;
-    color: #818181;
-    display: block;
-    transition: 0.5s;
-}
-
-.sidenav a:hover {
-    color: #f1f1f1;
-}
-
-.sidenav .closebtn {
-    position: absolute;
-    top: 0;
-    right: 25px;
-    font-size: 36px;
-    margin-left: 50px;
-}
-
-@media screen and (max-height: 450px) {
-  .sidenav {padding-top: 15px;}
-  .sidenav a {font-size: 18px;}
-}
 </style>
--- src/components/comment.vue
+++ src/components/comment.vue
@@ -1,7 +1,9 @@
 <template lang="html">
-  <div class="comment">
-    <p>By {{user}} {{created}}</p>
-    <p>{{content}}</p>    
+  <div class="comment">   
+    <span style="font-size:30px;cursor:pointer" v-on:click="openNav()">&#9776; open</span>
+    <div id="mySidenav" class="sidenav">
+      <a href="javascript:void(0)" class="closebtn" v-on:click="closeNav()">&times;</a>
+    </div> 
   </div>
 </template>
 
@@ -14,20 +16,63 @@ export default {
       created: '',
       content: ''
     }
+  },
+  methods: {
+    openNav() {
+        document.getElementById("mySidenav").style.width = "73%";
+    },
+    closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+    }
   }
 }
 </script>
 
 <style scoped lang="css">
-  .comment {
-    background: #f1c7c7;
-    padding: 20px 10px;
-    margin-bottom: 20px;
-  }
   h4 {
     margin: 5px 0;
   }
   p {
     margin: 5px 0;
   }
+
+.sidenav {
+    height: 100%;
+    width: 0;
+    position: fixed;
+    z-index: 1;
+    top: 0;
+    left: 0;
+    /* background-color: white; */
+    background: linear-gradient(to bottom right, white,rgb(93, 250, 250));
+    overflow-x: hidden;
+    transition: 0.8s;
+    padding-top: 60px;
+}
+
+.sidenav a {
+    padding: 8px 8px 8px 32px;
+    text-decoration: none;
+    font-size: 25px;
+    color: #818181;
+    display: block;
+    transition: 0.5s;
+}
+
+.sidenav a:hover {
+    color: #f1f1f1;
+}
+
+.sidenav .closebtn {
+    position: absolute;
+    top: 0;
+    right: 25px;
+    font-size: 36px;
+    margin-left: 50px;
+}
+
+@media screen and (max-height: 450px) {
+  .sidenav {padding-top: 15px;}
+  .sidenav a {font-size: 18px;}
+}
 </style>
